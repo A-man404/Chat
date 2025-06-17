fab2080c7a55388769e016868e72cf6461258a58
A-man404 <amansaini04a@gmail.com>
2025-06-17 18:09:33 +0530

    Made Changes

--- src/main/kotlin/service/UserService.kt
+++ src/main/kotlin/service/UserService.kt
@@ -1,13 +1,13 @@
 package com.example.service
 
 import com.example.model.ApiResponse
+import com.example.model.FullUser
 import com.example.model.UserResponse
 import com.example.repository.UserRepository
 import io.ktor.http.*
 
 class UserService {
     val userRepository = UserRepository()
-
 
     fun listAllUsers(): ApiResponse<List<UserResponse>> {
         return try {
@@ -28,5 +28,83 @@ class UserService {
             )
         }
     }
+
+    fun getProfile(id: Int): ApiResponse<FullUser?> {
+        return try {
+
+            if (!userRepository.idExists(id)) {
+                ApiResponse(
+                    data = null,
+                    success = false,
+                    statusCode = HttpStatusCode.NotFound.value,
+                    message = "This user doesnt exist"
+                )
+            } else {
+                val user = userRepository.findUserById(id)
+                ApiResponse(
+                    data = user,
+                    success = false,
+                    statusCode = HttpStatusCode.OK.value,
+                    message = "Profile Fetched Successfully"
+                )
+            }
+        } catch (e: Exception) {
+            ApiResponse(
+                data = null,
+                success = false,
+                statusCode = HttpStatusCode.InternalServerError.value,
+                message = "Error Occurred: ${e.message}"
+            )
+        }
+    }
+
+    fun searchUser(query: String): ApiResponse<List<FullUser>?> {
+        return try {
+            val user = userRepository.findUsersByQuery(query)
+            ApiResponse(
+                data = user,
+                success = true,
+                statusCode = HttpStatusCode.OK.value,
+                message = "Profile Fetched Successfully"
+            )
+
+        } catch (e: Exception) {
+            ApiResponse(
+                data = null,
+                success = false,
+                statusCode = HttpStatusCode.InternalServerError.value,
+                message = "Error Occurred: ${e.message}"
+            )
+        }
+    }
+
+    fun getProfileByUsername(name: String): ApiResponse<FullUser?> {
+        return try {
+            if (!userRepository.usernameExists(name)) {
+                ApiResponse(
+                    data = null,
+                    success = false,
+                    statusCode = HttpStatusCode.NotFound.value,
+                    message = "This user doesnt exist"
+                )
+            } else {
+                val user = userRepository.findUserByUsername(name)
+                ApiResponse(
+                    data = user,
+                    success = false,
+                    statusCode = HttpStatusCode.OK.value,
+                    message = "Profile Fetched Successfully"
+                )
+            }
+        } catch (e: Exception) {
+            ApiResponse(
+                data = null,
+                success = false,
+                statusCode = HttpStatusCode.InternalServerError.value,
+                message = "Error Occurred: ${e.message}"
+            )
+        }
+    }
+
 
 }
