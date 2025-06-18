c610be091cb28aa5a924b85ee83b6a55c29e5ef1
A-man404 <amansaini04a@gmail.com>
2025-06-18 16:17:54 +0530

    Made Changes

--- src/main/kotlin/service/UserService.kt
+++ src/main/kotlin/service/UserService.kt
@@ -3,8 +3,10 @@ package com.example.service
 import com.example.model.ApiResponse
 import com.example.model.FullUser
 import com.example.model.UserResponse
+import com.example.plugins.emailSender
 import com.example.repository.UserRepository
 import io.ktor.http.*
+import kotlin.random.Random
 
 class UserService {
     val userRepository = UserRepository()
@@ -96,6 +98,89 @@ class UserService {
                     message = "Profile Fetched Successfully"
                 )
             }
+        } catch (e: Exception) {
+            ApiResponse(
+                data = null,
+                success = false,
+                statusCode = HttpStatusCode.InternalServerError.value,
+                message = "Error Occurred: ${e.message}"
+            )
+        }
+    }
+
+    fun sendVerificationOtp(id: Int): ApiResponse<String> {
+        return try {
+            val random = Random.nextInt(100000, 999999)
+
+            val user = userRepository.findUserById(id) ?: return ApiResponse(
+                data = null,
+                success = false,
+                statusCode = HttpStatusCode.InternalServerError.value,
+                message = "Error Occurred"
+            )
+
+
+            emailSender(
+                subject = "Email Verification",
+                sendTo = user.email,
+                message = "Hi ${user.firstName},\n" +
+                        "\n" +
+                        "Your one-time password (OTP) is ${random}.\n" +
+                        "\n" +
+                        "Please do not share this code with anyone.\n" +
+                        "\n" +
+                        "If you didn’t request this code, you can safely ignore this email or contact our support team.\n" +
+                        "\n" +
+                        "Thank you,\n" +
+                        "Void Chat Team"
+            )
+            ApiResponse(
+                data = random.toString(),
+                success = true,
+                statusCode = HttpStatusCode.OK.value,
+                message = "Email Sent Successfully"
+            )
+
+        } catch (e: Exception) {
+            ApiResponse(
+                data = null,
+                success = false,
+                statusCode = HttpStatusCode.InternalServerError.value,
+                message = "Error Occurred: ${e.message}"
+            )
+        }
+    }
+
+    fun verifyEmailOtp(id: Int, sentOtp: String, storeOtp: String): ApiResponse<String> {
+        return try {
+
+            if (!userRepository.idExists(id)) {
+                ApiResponse(
+                    data = "User Doesnt Exist",
+                    success = false,
+                    statusCode = HttpStatusCode.BadRequest.value,
+                    message = "User Doesnt exist"
+                )
+            } else {
+                if (sentOtp == storeOtp) {
+                    userRepository.changeUserVerification(id)
+                    ApiResponse(
+                        data = "Email Verified Successfully",
+                        success = true,
+                        statusCode = HttpStatusCode.OK.value,
+                        message = "Email Verified Successfully"
+                    )
+                } else {
+                    ApiResponse(
+                        data = "Incorrect Otp",
+                        success = false,
+                        statusCode = HttpStatusCode.OK.value,
+                        message = "Email Unverified"
+                    )
+                }
+            }
+
+
         } catch (e: Exception) {
             ApiResponse(
                 data = null,
